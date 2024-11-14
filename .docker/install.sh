b600e3e8d2ee25ea5fbeed3a874ed95c0d7a6f07
lygris <lygriz@gmail.com>
2024-11-14 17:32:43 -0600

    fix

--- .docker/install.sh
+++ .docker/install.sh
@@ -13,6 +13,7 @@ fi
 
 curl https://raw.githubusercontent.com/lygris/Pihole-Unbound/refs/heads/main/.docker/docker-compose.yaml > $path2/docker-compose.yaml
 touch /home/pi/.firewalla/config/dnsmasq_local/00-config.conf
+touch /home/pi/.firewalla/config/dnsmasq_local/pihole
 echo "add-subnet=32,128
 add-mac" > /home/pi/.firewalla/config/dnsmasq_local/00-config.conf
 
