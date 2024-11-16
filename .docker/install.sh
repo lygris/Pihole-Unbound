e9b7ddd25773dd53ac73cf4efc01eaed5704d4d7
lygris <lygriz@gmail.com>
2024-11-16 14:22:48 -0600

    updates

--- .docker/install.sh
+++ .docker/install.sh
@@ -19,27 +19,17 @@ add-mac" > /home/pi/.firewalla/config/dnsmasq_local/00-config.conf
 
  
 cd $path2
+echo address=/pihole/172.16.0.2 > /home/pi/.firewalla/config/dnsmasq_local/pihole
+sudo systemctl restart firerouter_dns
 sudo systemctl start docker
 sudo docker-compose pull
 sudo docker-compose up --no-start
-sudo ip route add 172.16.0.0/24 dev br-$(sudo docker network inspect pi-hole_default |jq -r '.[0].Id[0:12]') table lan_routable
-sudo ip route add 172.16.0.0/24 dev br-$(sudo docker network inspect pi-hole_default |jq -r '.[0].Id[0:12]') table wan_routable
+sudo ip route add 172.16.0.0/24 dev br-$(sudo docker network inspect pihole |jq -r '.[0].Id[0:12]') table lan_routable
+sudo ip route add 172.16.0.0/24 dev br-$(sudo docker network inspect pihole |jq -r '.[0].Id[0:12]') table wan_routable
 sudo docker-compose up --detach
 
 
 sudo docker ps
-
-echo address=/pihole/172.16.0.2 > /home/pi/.firewalla/config/dnsmasq_local/pihole
-sudo systemctl restart firerouter_dns
-sudo docker restart pihole
-
-echo -n "Starting docker (this can take ~ one minute)"
-while [ -z "$(sudo docker ps | grep pihole | grep -o Up)" ]
-do
-        echo -n "."
-        sleep 2s
-done
-echo "Done"
 
 path3=/home/pi/.firewalla/config/post_main.d
 if [ ! -d "$path3" ]; then
@@ -53,13 +43,12 @@ sudo ipset create -! docker_lan_routable_net_set hash:net
 sudo ipset add -! docker_lan_routable_net_set 172.16.0.0/24
 sudo ipset create -! docker_wan_routable_net_set hash:net
 sudo ipset add -! docker_wan_routable_net_set 172.16.0.0/24
-sudo systemctl start docker-compose@pi-hole
+sudo systemctl start docker-compose@pihole
 sudo rmmod br_netfilter" > /home/pi/.firewalla/config/post_main.d/start_pihole.sh
 
 chmod a+x /home/pi/.firewalla/config/post_main.d/start_pihole.sh
 
 echo -n "Restarting docker"
-sudo docker start pihole
 sudo rmmod br_netfilter
 while [ -z "$(sudo docker ps | grep pihole | grep Up)" ]
 do
