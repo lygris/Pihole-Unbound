7a2c7d96ee90b889f2c6238e3165960049679b2e
lygris <lygriz@gmail.com>
2025-02-13 17:55:06 -0600

    Change to cloudflared

--- .docker/update.sh
+++ .docker/update.sh
@@ -1,10 +1,10 @@
-ipv6=$(ip -6 route show | grep 256 |  awk '{print $1}' | grep / | grep -v fe | awk 'ORS=","')
-ipv4=$(ip -4 route show | grep / | grep br | awk '{print $1}'| awk 'ORS=","')
-ipv6PD=$(ip -6 a show dev br2 | grep /64 | grep -v fe | awk '{print $2}' | awk -F: '{ print $1":"$2":"$3":"$4+10"::/64" }')
-routes=$ipv4$ipv6$ipv6PD
+export ipv6=$(ip -6 route show | grep 256 |  awk '{print $1}' | grep / | grep -v fe | awk 'ORS=","')
+export ipv4=$(ip -4 route show | grep / | grep br | awk '{print $1}'| awk 'ORS=","')
+export ipv6PD=$(ip -6 a show dev br2 | grep /64 | grep -v fe | awk '{print $2}' | awk -F: '{ print $1":"$2":"$3":"$4+10"::/64" }')
+export routes=$ipv4$ipv6$ipv6PD
 echo "routes=$routes" > .env
 echo "ipv6PD=$ipv6PD" >> .env
-echo "#!/bin/bash
+echo '#!/bin/bash
 # v1.0
 sudo systemctl start docker
 sudo ipset create -! docker_lan_routable_net_set hash:net
@@ -12,11 +12,15 @@ sudo ipset add -! docker_lan_routable_net_set 172.16.0.0/24
 sudo ipset create -! docker_wan_routable_net_set hash:net
 sudo ipset add -! docker_wan_routable_net_set 172.16.0.0/24
 sudo ipset create -! docker_lan_routable_net_set6 hash:net family inet6
-sudo ipset add -! docker_lan_routable_net_set6 $ipv6PD
+sudo ipset add -! docker_lan_routable_net_set6 '$ipv6PD'
 sudo ipset create -! docker_lan_routable_net_set6 hash:net family inet6
-sudo ipset add -! docker_wan_routable_net_set6 $ipv6PD
+sudo ipset add -! docker_wan_routable_net_set6 '$ipv6PD'
+sudo ip route add 172.16.0.0/24 dev br-$(sudo docker network inspect pihole |jq -r '.[0].Id[0:12]') table lan_routable
+sudo ip route add 172.16.0.0/24 dev br-$(sudo docker network inspect pihole |jq -r '.[0].Id[0:12]') table wan_routable
+sudo ip -6 route add '$ipv6PD' dev br-$(sudo docker network inspect pihole |jq -r '.[0].Id[0:12]') table lan_routable
+sudo ip -6 route add '$ipv6PD' dev br-$(sudo docker network inspect pihole |jq -r '.[0].Id[0:12]') table wan_routable
 sudo systemctl start docker-compose@pihole
-sudo rmmod br_netfilter" > /home/pi/.firewalla/config/post_main.d/start_pihole.sh
+sudo rmmod br_netfilter' > /home/pi/.firewalla/config/post_main.d/start_pihole.sh
 docker compose --env-file=.env pull
 docker compose --env-file=.env down
 docker compose --env-file=.env up -d
--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,6 @@
 FROM pihole/pihole:development
-RUN apk update && apk upgrade && apk add unbound
-COPY unbound.conf /etc/unbound/unbound.conf
-RUN mkdir -p /etc/unbound
-COPY unbound-run /etc/unbound/run
-RUN chmod +x /etc/unbound/run
-RUN touch /var/log/unbound.log
-RUN chmod 777 /var/log/unbound.log
-RUN sed -i '2i nohup bash -c unbound &' /usr/bin/start.sh
-RUN wget -S https://www.internic.net/domain/named.cache -O /etc/unbound/root.hints
-RUN sed -i '2i unbound-anchor -a /etc/unbound/root.key' /usr/bin/start.sh
-RUN chown -R unbound:unbound /etc/unbound
+RUN wget https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-amd64 -O /usr/bin/cloudflared
+RUN chmod +x /usr/bin/cloudflared
+RUN cloudflared service install eyJhIjoiY2JlOGIyOTNlZGJmOGZhMTlkMDc2OWI1ZGIxYmI3MGMiLCJ0IjoiNDNiMjc1ZTgtZTU3Yy00ZDQ0LWE0YzgtODFkNTZmZWM2NWZkIiwicyI6IlptUXpNek14TVRrdE5UTmhNeTAwT1RJeExXSXlNall0WkRObE16Um1PV000TURnNSJ9
+RUN sed -i '2i nohup bash -c cloudflared proxy-dns --port 5053 --upstream https://1.1.1.1/dns-query --upstream https://1.0.0.1/dns-query --address 0.0.0.0 &' /usr/bin/start.sh
 ENTRYPOINT [ "start.sh" ]
